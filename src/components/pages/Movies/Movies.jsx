db73f0432dd06b34bac1ba0ced1de5eb9649b4ba
KaterynaSlatina <131613660+KaterynaSlatina@users.noreply.github.com>
2023-12-23 21:21:48 +0200

    add movies

--- src/components/pages/Movies/Movies.jsx
+++ src/components/pages/Movies/Movies.jsx
@@ -1,28 +1,42 @@
 import Search from 'components/Search/Search';
 import { fetchSearchMovies } from 'components/fetchApi';
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Movies = () => {
-  const [movie, setMovie] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
 
-  const getSearchMovies = async () => {
+  const getSearchMovies = async searchQuery => {
     try {
-      const resp = await fetchSearchMovies();
-      setMovie(resp);
+      const resp = await fetchSearchMovies(searchQuery);
+      setMovies(resp);
     } catch (error) {
       console.error(error);
     }
   };
 
   useEffect(() => {
-    getSearchMovies();
-  }, [movie]);
+    if (query) {
+      getSearchMovies(query);
+    }
+  }, [query]);
 
   const submit = query => {
-    getSearchMovies(query);
+    setQuery(query);
   };
 
-  return <Search submit={submit} />;
+  return (
+    <>
+      <Search submit={submit} />
+
+      {movies.map(movie => (
+        <Link to={`movies/${movie.id}`} key={movie.id}>
+          {movie.title}
+        </Link>
+      ))}
+    </>
+  );
 };
 
 export default Movies;
